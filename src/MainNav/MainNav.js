7452246995dc8321dc7319ff068c9e0bfcadadb9
Mike Morris <michaelmorrisg@gmail.com>
2018-11-05 15:53:43 -0700

    initial commit

---
+++ src/MainNav/MainNav.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import './MainNav.css'
+
+class MainNav extends Component {
+
+
+    render(){
+        return(
+            <div>
+                <div className="MainNav-row1">
+                    <p><a href="">sign in</a> or <a href="">create an account</a></p>
+                    <p>My Cart</p>
+                </div>
+                <div className="MainNav-row2">
+                    <h1>ROOLEE</h1>
+                </div>
+                <div className="MainNav-row3">
+                    <nav>
+                        <ul>
+                            <li>NEW</li>
+                            <li>CLOTHING</li>
+                            <li>SHOES + ACCESSORIES</li>
+                            <li>OUTFITS</li>
+                            <li>MOM</li>
+                            <li>KIDS</li>
+                            <li>HOUSE + HOME</li>
+                            <li>SALE</li>
+                            <li>GIFTS</li>
+                            <li>FAQS</li>
+                            <li>MORE</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        )
+    }
+} export default MainNav
---
+++ src/TopMessage/TopMessage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TopMessage.css'
+
+export default function TopMessage(){
+    return(
+        <div className="TopMessage-main">
+            <p>Get 10% Off the Lounge Around Sweats when you buy the Top + Bottom Together!</p>
+        </div>
+    )
+}
