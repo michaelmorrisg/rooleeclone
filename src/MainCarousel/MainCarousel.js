4a616073cc92701d46ae0aa36f4dc9074b336527
Mike Morris <michaelmorrisg@gmail.com>
2018-11-08 10:34:59 -0700

    main image carousel is now a carousel

--- src/MainCarousel/MainCarousel.js
+++ src/MainCarousel/MainCarousel.js
@@ -2,13 +2,52 @@ import React, {Component} from 'react'
 import {Carousel} from 'react-responsive-carousel'
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './MainCarousel.css'
+import bells from '../Assets/bells.jpg'
+import main from '../Assets/main-text.jpg'
+import buffalo from '../Assets/buffalo.jpg'
 
 class MainCarousel extends Component {
+    constructor(){
+        super()
+        this.state = {
+            images : [bells,main,buffalo],
+            currentSlide : 0
+        }
+    }
+    advanceSlide(){
+        if(this.state.currentSlide < this.state.images.length -1){
+            this.setState({
+                currentSlide : this.state.currentSlide + 1
+            })
+        } else {
+            this.setState({
+                currentSlide: 0
+            })
+        }
+    }
+    regressSlide(){
+        if(this.state.currentSlide > 0){
+            this.setState({
+                currentSlide : this.state.currentSlide - 1
+            })
+        } else {
+            this.setState({
+                currentSlide : this.state.images.length -1
+            })
+        }
+    }
 
     render(){
         return(
             <div className="MainCarousel-main">
-                <img src={require("../Assets/main-text.jpg")} />
+                <div className="MainCarousel-carousel-arrow left" onClick={()=>this.advanceSlide()}>
+                    <img src={require('../Assets/left.png')}/>
+                </div>
+                <div className="white-background"></div>
+                <img src={this.state.images[this.state.currentSlide]} className="MainCarousel-image" />
+                <div className="MainCarousel-carousel-arrow right" onClick={()=>this.advanceSlide()}>
+                    <img src={require('../Assets/right.png')}/>
+                </div>
             </div>
         )
     }
