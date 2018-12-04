c90b065ff0115135d5cf471100fb1c04685b67e9
Mike Morris <michaelmorrisg@gmail.com>
2018-12-04 09:37:55 -0700

    chat bubble zindex is now 12

--- src/MobileMenu/MobileMenu.js
+++ src/MobileMenu/MobileMenu.js
@@ -7,7 +7,17 @@ class MobileMenu extends Component {
     render(){
 
         return(
-            <div className="MobileMenu-main" style={{display: this.props.expanded ? "block" : "none"}}>Menu</div>
+            <div className="MobileMenu-main" style={{display: this.props.expanded ? "block" : "none"}}>
+                <p><a href="">NEW</a></p>
+                <p><a href="">CLOTHING</a></p>
+                <p><a href="">SHOES + ACCESSORIES</a></p>
+                <p><a href="">OUTFITS</a></p>
+                <p><a href="">MOM</a></p>
+                <p><a href="">KIDS</a></p>
+                <p><a href="">HOUSE + HOME</a></p>
+                <p><a href="">GIFTS</a></p>
+                <p><a href="">FAQS</a></p>
+            </div>
         )
     }
 }
